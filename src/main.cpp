682d1ad887fe3aec4d4a42a53d03c03eb349e98b
iLyas <isbkch@gmail.com>
2021-02-23 19:03:03 -0500

    basic example

--- src/main.cpp
+++ src/main.cpp
@@ -1,121 +1,133 @@
-/**
-  Wakes up every 30sec to read data from the water sensor, when water is
-  detected, it connects to WiFi, then sends an alarm to an MQTT topic
-  @author MecaHumArduino
-  @version 1.0
-*/
-
 #include <Arduino.h>
 #include "secrets.h"
-#include <ESP8266WiFi.h>
-#include <PubSubClient.h>
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
 
 #define DEBUG true // switch to "false" for production
-WiFiClient espClient;
-PubSubClient client(espClient);
-
-#define NB_TRYWIFI 20 // WiFi connection retries
+Adafruit_MPU6050 mpu;
 
 // **************
 void loop();
 void setup();
-void connectToHass();
-void connectToWiFi();
 // **************
 
-void tick()
-{
-    // toggle state
-    int state = digitalRead(LED_BUILTIN); // get the current state of GPIO1 pin
-    digitalWrite(LED_BUILTIN, !state);    // set pin to the opposite state
-}
 
-/**
- * Establishes WiFi connection
- * @return
- */
-void connectToWiFi()
-{
-    int _try = 0;
-    WiFi.mode(WIFI_STA);
-    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-
-    if (DEBUG == true)
-    {
-        Serial.println("Connecting to Wi-Fi");
-    }
-
-    while (WiFi.status() != WL_CONNECTED)
-    {
-        delay(500);
-        Serial.print(".");
-        _try++;
-        if (_try >= NB_TRYWIFI)
-        {
-            if (DEBUG == true)
-            {
-                Serial.println("Impossible to connect WiFi, going to deep sleep");
-            }
-        }
-    }
-    if (DEBUG == true)
-    {
-        Serial.println("Connected to Wi-Fi");
-    }
-}
-
-/**
- * Establishes connection to Home Assistant MQTT Broker
- * @return
- */
-void connectToHass()
-{
-    client.setServer(MQTT_SERVER, 1883);
-
-    // Loop until we're reconnected
-    while (!client.connected())
-    {
-        if (DEBUG == true)
-        {
-            Serial.print("Attempting MQTT connection...");
-        }
-        // Attempt to connect
-        // If you do not want to use a username and password, change next line to
-        // if (client.connect("ESP8266Client")) {
-        if (client.connect("ESP8266Client", MQTT_USER, MQTT_PASSWORD))
-        {
-            if (DEBUG == true)
-            {
-                Serial.println("connected");
-            }
-        }
-        else
-        {
-            if (DEBUG == true)
-            {
-                Serial.print("failed, rc=");
-                Serial.print(client.state());
-                Serial.println(" try again in 5 seconds");
-            }
-            // Wait 5 seconds before retrying
-            delay(5000);
-        }
-    }
-}
-
-
-void setup()
+void setup(void)
 {
     // only print debug messages to serial if we're in debug mode
     if (DEBUG == true)
     {
         Serial.print("Waking up ");
     }
-
     Serial.begin(115200);
+    while (!Serial) {
+        delay(10); // will pause Zero, Leonardo, etc until serial console opens
+    }
 
+    Serial.println("Adafruit MPU6050 test!");
+
+    // Try to initialize!
+    if (!mpu.begin())
+    {
+        Serial.println("Failed to find MPU6050 chip");
+        while (1)
+        {
+            delay(10);
+        }
+    }
+    Serial.println("MPU6050 Found!");
+
+    mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+    Serial.print("Accelerometer range set to: ");
+    switch (mpu.getAccelerometerRange())
+    {
+    case MPU6050_RANGE_2_G:
+        Serial.println("+-2G");
+        break;
+    case MPU6050_RANGE_4_G:
+        Serial.println("+-4G");
+        break;
+    case MPU6050_RANGE_8_G:
+        Serial.println("+-8G");
+        break;
+    case MPU6050_RANGE_16_G:
+        Serial.println("+-16G");
+        break;
+    }
+    mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+    Serial.print("Gyro range set to: ");
+    switch (mpu.getGyroRange())
+    {
+    case MPU6050_RANGE_250_DEG:
+        Serial.println("+- 250 deg/s");
+        break;
+    case MPU6050_RANGE_500_DEG:
+        Serial.println("+- 500 deg/s");
+        break;
+    case MPU6050_RANGE_1000_DEG:
+        Serial.println("+- 1000 deg/s");
+        break;
+    case MPU6050_RANGE_2000_DEG:
+        Serial.println("+- 2000 deg/s");
+        break;
+    }
+
+    mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+    Serial.print("Filter bandwidth set to: ");
+    switch (mpu.getFilterBandwidth())
+    {
+    case MPU6050_BAND_260_HZ:
+        Serial.println("260 Hz");
+        break;
+    case MPU6050_BAND_184_HZ:
+        Serial.println("184 Hz");
+        break;
+    case MPU6050_BAND_94_HZ:
+        Serial.println("94 Hz");
+        break;
+    case MPU6050_BAND_44_HZ:
+        Serial.println("44 Hz");
+        break;
+    case MPU6050_BAND_21_HZ:
+        Serial.println("21 Hz");
+        break;
+    case MPU6050_BAND_10_HZ:
+        Serial.println("10 Hz");
+        break;
+    case MPU6050_BAND_5_HZ:
+        Serial.println("5 Hz");
+        break;
+    }
+
+    Serial.println("");
+    delay(100);
 }
 
 void loop()
 {
+
+    /* Get new sensor events with the readings */
+    sensors_event_t a, g, temp;
+    mpu.getEvent(&a, &g, &temp);
+
+    /* Print out the values */
+    Serial.print("Acceleration X: ");
+    Serial.print(a.acceleration.x);
+    Serial.print(", Y: ");
+    Serial.print(a.acceleration.y);
+    Serial.print(", Z: ");
+    Serial.print(a.acceleration.z);
+    Serial.println(" m/s^2");
+
+    Serial.print("Rotation X: ");
+    Serial.print(g.gyro.x);
+    Serial.print(", Y: ");
+    Serial.print(g.gyro.y);
+    Serial.print(", Z: ");
+    Serial.print(g.gyro.z);
+    Serial.println(" rad/s");
+
+    Serial.println("");
+    delay(1000);
 }
